e688503aca281a40e2d217cc4b53e8706d80a994
Wayne Bloss <waynebloss@gmail.com>
2022-08-17 19:17:00 -0400

    Trying to exclude files from deno.land.

--- _tools/build_npm.ts
+++ _tools/build_npm.ts
@@ -1,5 +1,5 @@
 /**
- * @file Run with `deno run -A scripts/build_npm.ts 1.0.0`
+ * @file Run with `deno run -A build_npm.ts 1.0.0`
  */
 import { build, emptyDir } from "https://deno.land/x/dnt/mod.ts";
 
